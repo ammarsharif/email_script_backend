9f0aaea7083e797670e6bb7702a5c10dd6db5066
ammarsharif <ammarsharif377@gmail.com>
2024-06-24 11:33:09 +0500

    subscription plan 2

--- routes/profile.js
+++ routes/profile.js
@@ -4,6 +4,20 @@ const Subscription = require('../models/Subscription');
 
 const router = express.Router();
 
+router.get('/api/profile', async (req, res) => {
+  try {
+    console.log(req.query,'REQUEST QUERY::::');
+    const profile = await Profile.findOne({ emailAddress: req.query.email });
+    if (!profile) {
+      return res.status(404).json({ error: 'Profile not found' });
+    }
+    res.status(200).json(profile);
+  } catch (error) {
+    console.error('Error fetching profile:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
 router.post('/', async (req, res) => {
   const { token, tokenStatus, increment } = req.body;
 
